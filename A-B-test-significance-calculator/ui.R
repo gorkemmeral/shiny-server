5499f52e9f05680ef4926377369aff3182b6a9d6
Gorkem Meral <grkmmrl@gmail.com>
2018-03-10 23:33:16 +0000

    Update ui.R

--- A-B-test-significance-calculator/ui.R
+++ A-B-test-significance-calculator/ui.R
@@ -8,16 +8,18 @@ shinyUI(pageWithSidebar(
 	sidebarPanel(
 	span("Use this calculator to validate your hypothesis. Enter the results of your experiment below and click on the 'Calculate' button."),
 	br(),
+	span("This is a one-sided calculator; if the conversion rate of the variation group (B) is lower than the control group (A), no conclusive statement can be made.")
+	br(), br(),
 	span("Or return to", a(href = "http://gorkemmeral.com", "gorkemmeral.com")),
 	hr(),
 	selectInput("variable", "Select confidence level for significance",
 	list("95%" = "95",
 	"99%" = "99")), 
 	
-	numericInput("Avisitors", "Enter number of visitor for the control group", 1000),
-	numericInput("Bvisitors", "Enter number of visitors for the variation group", 1000),
-	numericInput("Aconversions", "Enter number of conversions for the control group", 200),
-	numericInput("Bconversions", "Enter number of conversions for the variation group", 200),
+	numericInput("Avisitors", "Enter number of visitor for the control group (A)", 1000),
+	numericInput("Bvisitors", "Enter number of visitors for the variation group (B)", 1000),
+	numericInput("Aconversions", "Enter number of conversions for the control group (A)", 200),
+	numericInput("Bconversions", "Enter number of conversions for the variation group (B)", 200),
 	
 	submitButton("Calculate"),
 	hr(),
