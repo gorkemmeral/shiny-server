a6bc4e73a19e3f2121298b1eafe1ddfda1df6b46
Gorkem Meral <grkmmrl@gmail.com>
2018-03-10 20:10:05 +0000

    Update ui.R

--- A-B-test-significance-calculator/ui.R
+++ A-B-test-significance-calculator/ui.R
@@ -19,8 +19,7 @@ shinyUI(pageWithSidebar(
 	
 	submitButton("Calculate"),
 	hr(),
-<input type="button" value="Return to gorkemmeral.com" onclick="window.location.href='http://gorkemmeral.com'" />
-,
+	span("Return to", a(href = "http://gorkemmeral.com", "gorkemmeral.com")),
 	br(),
 	span("Code forked from", a(href = "https://github.com/Jverma/Significance-in-A-B-testing", "here"))
 
