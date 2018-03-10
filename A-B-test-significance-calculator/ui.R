fc5d54caa99ed6048aef8bbce12fd904e84b5e01
Gorkem Meral <grkmmrl@gmail.com>
2018-03-10 21:40:17 +0000

    Update ui.R

--- A-B-test-significance-calculator/ui.R
+++ A-B-test-significance-calculator/ui.R
@@ -32,7 +32,7 @@ shinyUI(pageWithSidebar(
 		verbatimTextOutput("Confidence_intervalA"),
 		verbatimTextOutput("Confidence_intervalB"),
 		verbatimTextOutput("Zscore"),
-		verbatimTextOutput("pvalue")
+		verbatimTextOutput("pvalue"),
 		htmlOutput("statement")
 	)
 
