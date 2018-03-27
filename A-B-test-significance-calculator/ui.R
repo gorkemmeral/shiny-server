c1b66f204b4332a53d67f247a03c33dd2293cbf7
Gorkem Meral <grkmmrl@gmail.com>
2018-03-27 20:51:23 +0100

    Update ui.R

--- A-B-test-significance-calculator/ui.R
+++ A-B-test-significance-calculator/ui.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 shinyUI(pageWithSidebar(
-	tags$meta(name = "Description", content = 'Statistical significance calculator for A/B test results. Enter your test results, calculate the p-value and z-score, see if the results are statistically significant."),
+	tags$head(tags$meta(name = "Description", content = "Statistical significance calculator for A/B test results. Enter your test results, calculate the p-value and z-score, see if the results are statistically significant.")),
 	#Appl title
 	headerPanel("A/B Test Statistical Significance Calculator"),
 	sidebarPanel(
